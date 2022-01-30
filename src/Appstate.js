5bf76192ad1bac3c7a6dcf8816c1155a72d32147
Hammad563 <munirhammad786@gmail.com>
2022-01-29 20:07:53 -0500

    Finished Authentication/Navigation

--- src/Appstate.js
+++ src/Appstate.js
@@ -3,34 +3,46 @@ import React, { useReducer, useContext } from "react";
 // Initial State
 
 const initialState = {
-    url: "http://notesrailsbackend.herokuapp.com"
-}
+  url: "http://notesrailsbackend.herokuapp.com",
+  token: null,
+  username: null,
+};
 
-
-// Reducer 
+// Reducer
 const reducer = (state, action) => {
-
-    switch(action.type){
-
-        default: {
-            return state
-        }
+  let newState;
+  switch (action.type) {
+    case "auth":
+      {
+        newState = { ...state, ...action.payload };
+        return newState;
+      }
+      break;
+    case "logout": {
+      newState = { ...state, token: null, username: null };
+      window.localStorage.removeItem("auth");
+      return newState;
     }
-}
+    default: {
+      return state;
+    }
+  }
+};
 
 /// app context
-const AppContext = React.createContext(null)
+const AppContext = React.createContext(null);
 
 export const AppState = (props) => {
-    const [state, dispatch] = useReducer(reducer, initialState)
-    return <AppContext.Provider value={{state, dispatch}}>{props.children}</AppContext.Provider>
-}
-
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
 
 // useAppState Hook
 
 export const useAppState = () => {
-    return React.useContext(AppContext)
-}
-
-
+  return React.useContext(AppContext);
+};
